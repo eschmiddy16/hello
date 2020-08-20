f44f71548bb14f95422e2d4e67c903ebd1f52bba
Ethan Schmidt <eschmidt16@huskers.unl.edu>
2020-08-20 16:58:44 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ *Author: Ethan Schmidt
+ * Date: 2020/08/20
+ *
+ * A program that prints my name.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Ethan Schmidt\n");
+
+    return 0;
+}
